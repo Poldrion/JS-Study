51d8808d43750a833cdce6dabc92eae5d8d29500
Poldrion <Romashka2007@inbox.ru>
2023-02-24 20:38:27 +0400

    Доработка сайта

--- script.js
+++ script.js
@@ -84,3 +84,70 @@ const eventFunc = (e) => {
 for (let i = 0; i < choosedEl.length; i++) {
   choosedEl[i].addEventListener("click", eventFunc);
 }
+
+// const timeIsOver = () => {
+//   alert("Время вышло!");
+// };
+
+// // setTimeout(timeIsOver, 2000);
+// // const alarm = setInterval(timeIsOver, 3000);
+
+// const alarm = setInterval(() => {
+//   let wantToSleep = confirm("Хотите ли вы спать?");
+//   if (wantToSleep) {
+//     console.log("tic");
+//   } else {
+//     clearInterval(alarm);
+//   }
+// }, 3000);
+
+const postsBlock = document.querySelector(".posts_block-container");
+const showPostsBTN = document.querySelector(".posts_block button");
+
+function addPost(title, body) {
+  const postTitle = document.createElement("h3");
+  const postBody = document.createElement("span");
+  const postItem = document.createElement("p");
+
+  postTitle.innerText = title;
+  postBody.innerText = body;
+
+  postItem.append(postTitle, postBody);
+  postsBlock.append(postItem);
+}
+
+function getPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      for (item of data) {
+        addPost(item.title, item.body);
+      }
+      console.log(data);
+    })
+    .catch((err) => console.log(err.message));
+}
+
+// function createPost(title, body, userId) {
+//   fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       title: title,
+//       body: body,
+//       userId: userId,
+//     }),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   })
+//     .then((res) => {
+//       console.log(res);
+//       return res.json();
+//     })
+//     .catch((err) => console.log(err.message));
+// }
+// createPost("title", "body", 15);
+
+showPostsBTN.onclick = () => {
+  getPosts();
+};
